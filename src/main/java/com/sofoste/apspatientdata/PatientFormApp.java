a42d0e172f02c4cd91a8b5477ffe142a1aad19aa
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2023-03-15 00:01:34 +0100

    Update logo & layout
    add logo
    
    Co-Authored-By: Stephane Sob Fouodji <stefan-kruger7@hotmail.com>

--- src/main/java/com/sofoste/apspatientdata/PatientFormApp.java
+++ src/main/java/com/sofoste/apspatientdata/PatientFormApp.java
@@ -2,7 +2,9 @@ package com.sofoste.apspatientdata;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
